40c27e75e0fb82d0715b4114d026696bac7bd4ff
kofi12 <kofi12@my.yorku.ca>
2023-04-13 22:21:56 -0400

    reworking models

--- bill_auto/migrations/0001_initial.py
+++ bill_auto/migrations/0001_initial.py
@@ -16,7 +16,6 @@ class Migration(migrations.Migration):
                 ("name", models.CharField(max_length=50)),
                 ("birthday", models.DateField()),
                 ("admission_date", models.DateField()),
-                ("discharge_date", models.DateField(blank=True)),
                 ("rent", models.DecimalField(decimal_places=2, max_digits=10)),
             ],
         ),
