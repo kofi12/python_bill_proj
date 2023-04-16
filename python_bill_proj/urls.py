6c5747cfe2a96cceaf2865efc11fa3b1de827fa3
kofi12 <kofi12@my.yorku.ca>
2023-04-16 17:39:39 -0400

    file structure

--- python_bill_proj/urls.py
+++ python_bill_proj/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('bill_auto.urls')),
     path('dashboard/', include('bill_auto.urls')),
-]   
+]
+
+urlpatterns += staticfiles_urlpatterns()
