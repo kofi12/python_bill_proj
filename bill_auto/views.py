b564238a313ab55b147b82538ca0735a2fb38397
kofi12 <kofi12@my.yorku.ca>
2023-04-18 21:26:07 -0400

    I think i've figured something out

--- bill_auto/views.py
+++ bill_auto/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Resident
 
 # Create your views here.
 def greet_view(request):
     return render(request, 'index.html')
 
-def dash_view(request):
-    return render(request, 'dashboard.html')
+def dash_view(request, id):
+    resident = Resident.objects.get(pk=id)
+    return render(request, 'dashboard.html', {'resident': resident})
